413248d17035fee340386f3c1917de73243cc1e1
Bukunmi-israel <86633403+Bukunmi-israel@users.noreply.github.com>
2021-07-04 22:24:03 +0100

    First page
    Of course

---
+++ hitjava.js
@@ -0,0 +1,19 @@
+
+function hide(){
+	document.getElementById("zz").style.display = "block"
+	}
+function show(){
+	document.getElementById("text").style.display = "none"
+	
+}
+function show2(){
+	document.getElementById("text").style.display = "block"}
+function hide2(){
+document.getElementById("zz").style.display = "none"
+}
+if(document.getElementById("text")){
+	document.getElementById("zz").style.display = "none"
+}
+if(document.getElementById("zz")){
+	document.getElementById("text").style.display = "none"
+}
